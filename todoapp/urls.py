2a04b55ac2eb1d741817af941185852eaf6bc95f
Sajal <sajaldebnath02@gmail.com>
2022-05-09 11:28:46 +0600

    todo delete and update of completed field is Done.

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import TodoView, TodoDetailView, TodoUpdateView
+from .views import TodoView, TodoDetailView, TodoUpdateView, ToDoDeleteView, TodoCompletedView
 
 urlpatterns = [
     path('todos_get/', TodoView.as_view(), name='todos'),
     path('todos_detail/<str:pk>/', TodoDetailView.as_view(), name='todo_details'),
-    path('todo_update/<str:pk>/', TodoUpdateView.as_view(), name='todo_update')
+    path('todo_update/<str:pk>/', TodoUpdateView.as_view(), name='todo_update'),
+    path('todo_delete/<str:pk>/', ToDoDeleteView.as_view(), name='todo_delete'),
+    path('todo_completed/<str:pk>/', TodoCompletedView.as_view(), name='todo_completed')
 ]
 
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
@@ -31,6 +33,21 @@ class TodoDetailView(APIView):
 class TodoUpdateView(APIView):
     def post(self, request, pk):
         Task.objects.filter(id=pk).update(title=request.data['title'])
-        return Response(status=status.HTTP_200_OK)
+        return Response({'msg': 'Data updated.'}, status=status.HTTP_200_OK)
 
+
+class ToDoDeleteView(APIView):
+    def delete(self, request, pk):
+        try:
+            todo = Task.objects.get(pk=pk)
+            todo.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except ObjectDoesNotExist:
+            return Response({'msg': 'Does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class TodoCompletedView(APIView):
+    def post(self, request, pk):
+        Task.objects.filter(id=pk).update(completed=request.data['completed'])
+        return Response({'msg': 'Data updated.'}, status=status.HTTP_200_OK)
 
