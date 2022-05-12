233665055b5d16bf3f625a37da97ea627d2a87ef
Sajal <sajaldebnath02@gmail.com>
2022-05-12 19:08:22 +0600

    Article Model created.

--- todoapp/models.py
+++ todoapp/models.py
@@ -8,4 +8,9 @@ class Task(models.Model):
     completed = models.BooleanField(default=False, blank=True)
 
 
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=400)
+
+
 
