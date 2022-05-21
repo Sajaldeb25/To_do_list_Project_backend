5c87cf917c3f8d3f50149b5ee4d879287cf76406
Sajal <sajaldebnath02@gmail.com>
2022-05-21 21:51:18 +0600

    Sort Artile by in Admin by title.

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -4,6 +4,10 @@ from .models import Task, Article
 # Register your models here.
 
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
 admin.site.register(Task)
 admin.site.register(Article)
 
