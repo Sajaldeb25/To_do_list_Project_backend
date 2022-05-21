4ad3f00184707332a2952770380542ef571a87a9
Sajal <sajaldebnath02@gmail.com>
2022-05-22 00:43:21 +0600

    different types of show.

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -5,11 +5,14 @@ from .models import Task, Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    # list_display = ['id']  # show id in admin
+    # list_display = ['title']  # show title in admin
+    list_display = ['title', 'id']  # show both title and id in admin
+    search_fields = ['title', 'content']  # search by title and content
 
 
 admin.site.register(Task)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 
 
 
