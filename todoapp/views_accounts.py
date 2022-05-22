e23ecffcbb1ab03dc082b3234acff6c95f8ef603
Sajal <sajaldebnath02@gmail.com>
2022-05-23 00:42:22 +0600

    Login done.

--- todoapp/views_accounts.py
+++ todoapp/views_accounts.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 
 def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        # print(username, password)
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            context = {
+                "error": "Username and password is invalid."
+            }
+            return render(request, "accounts/login.html", context=context)
+        else:
+            login(request, user)
+            return redirect('/')
     return render(request, "accounts/login.html", {})
 
 
